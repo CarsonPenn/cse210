a54a354e80ee047bd899e9005509a692a837ac0b
CarsonPenn <carsonpenn@byui.edu>
2024-09-25 09:09:03 -0600

    909

---
+++ sandbox/Sandbox/counter/Program.cs
@@ -0,0 +1,15 @@
+﻿// namespace counter;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello, World!");
+//     }
+// }
+Console.WriteLine("Hello, World!");
+
+for ( int i=1; i<=10; i += 1)
+{
+    Console.WriteLine(i);
+}
