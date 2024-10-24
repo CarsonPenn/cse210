5869eeb929c390a24c1c501f7d7ab08b0d38b1fa
CarsonPenn <carsonpenn@byui.edu>
2024-10-23 18:37:08 -0600

    added a sandbox titled codingaround to use for when i am watching tutuorials

--- sandbox/Sandbox/codingaround/Program.cs
+++ sandbox/Sandbox/codingaround/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Title = "Terminal.8976342";
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WindowHeight = 40;
+        
+        Console.WriteLine("PWD prtection enabled, bypass using credentialed tron access.");
+        Console.WriteLine("");
+        Console.ReadLine();
+        Console.WriteLine("access granted interface x934.ab-4k89 booting \n system processing \n439rieu)***&^%$#");
+        Console.WriteLine("system booting");
+        Console.WriteLine("interface loaded");
+        Console.ReadLine();
+         Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("command acknowledged");
+
+    
+    
     }
 }
